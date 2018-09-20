6aa70e268dff1eb57109a322176c797d3714af7a
Isabelle Galvão <isabellegalvao@MacBook-Pro-de-Isabelle.local>
2018-09-20 09:08:11 -0300

    update pastas dia 5 e 4

--- 4/ex-scroll/script/script.js
+++
@@ -1,9 +0,0 @@
-const arrowTop = document.getElementById('arrowTop');
-
-arrowTop.onclick = function() {
-    window.scrollTo(pageXOffset, 0);
-};
-
-window.addEventListener('scroll', function() {
-    arrowTop.hidden = (pageYOffset < document.documentElement.clientHeight);
-});
