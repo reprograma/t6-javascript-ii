5fa11e3bd09fa6a531de3890d71cb874ce6046b2
Isabelle Galvão <isabellegalvao@MacBook-Pro-de-Isabelle.local>
2018-09-21 13:35:50 -0300

    update pasta 5

--- 4/exemplo-1-twitter/script/script2.js
+++ 4/exemplo-1-twitter/script/script2.js
@@ -1,7 +1,7 @@
 const inputTweet = document.getElementById("tweetComposerInput")
 const buttonCreateTweet = document.querySelector(".tweet-composer__button")
-
 const tweetsTimeline = document.querySelector(".tweets-timeline")
+const contador = document.getElementById("tweetComposerCounter");
 
 buttonCreateTweet.addEventListener("click", function(e){
     e.preventDefault();
@@ -9,6 +9,10 @@ buttonCreateTweet.addEventListener("click", function(e){
     if(inputTweet.value === undefined || inputTweet.value === null || inputTweet.value === "" || inputTweet.value === " "){
         return false;
     }
+
+    const date = new Date();
+    const month = date.toLocaleString("pt-br", { month: "short" });
+    const day = date.getDate();
 
     //criei a div que abraça todo o conteúdo
     const div = document.createElement("div")
@@ -19,7 +23,7 @@ buttonCreateTweet.addEventListener("click", function(e){
     header.className = "tweets-timeline__header";
     header.innerHTML = `<span class="tweets-timeline__name">Isabelle Galvão</span>\
         <span class="tweets-timeline__username">@galvaoiisabelle</span>\
-        <span class="tweets-timeline__date">1 de ago</span>`
+        <span class="tweets-timeline__date">${day} de ${month}</span>`
 
     //criei um elemento novo pro tweet
     const newTweetP = document.createElement("p");
@@ -39,7 +43,9 @@ buttonCreateTweet.addEventListener("click", function(e){
     //coloquei a div na timeline
     tweetsTimeline.insertBefore(div, tweetsTimeline.childNodes[0]);
 
-    inputTweet.value = ":)";
+    inputTweet.value = "";
+
+    contador.innerHTML = 280;
 
     const botaoExcluir = document.querySelector(".tweets-timeline__delete-button");
 
@@ -49,7 +55,38 @@ buttonCreateTweet.addEventListener("click", function(e){
         console.log("oi")
     })
 
+})
 
+//keydown
+inputTweet.addEventListener("keyup", function(){
+    const textBox = this;
+    const maxLength = 280;
+    const caracteresDisponiveis = maxLength -  textBox.value.length;
 
+    contador.innerHTML = caracteresDisponiveis;
+    buttonCreateTweet.disabled = false;
+
+    if(caracteresDisponiveis <= 15){
+        contador.style.color = "red";
+    }else{
+        contador.style.color = "white";
+    }
+    
+    if (caracteresDisponiveis < 0){
+        buttonCreateTweet.disabled = true;
+        contador.style.color = "red";
+    }else{
+        contador.style.color = "white";
+        buttonCreateTweet.disabled = false;
+    }
+
+    if(caracteresDisponiveis <= 15 && caracteresDisponiveis >=0){
+        contador.style.color = "red";
+    }else if(caracteresDisponiveis < 0){
+        buttonCreateTweet.disabled = true;
+        contador.style.color = "red";
+    }else{
+        contador.style.color = "white";
+    }
 
 })
